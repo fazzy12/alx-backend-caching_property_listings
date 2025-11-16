8b65572f33a48f8e8b784d40d3de41c3c4b13ea4
fazzy12 <kaluifeanyi00@gmail.com>
2025-11-16 04:31:01 +0100

    feat: Implement view-level caching for property list

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15)
+def property_list(request):
+    """
+    Renders a list of all properties, with the entire view response cached for 15 minutes.
+    """
+    properties = Property.objects.all()
+    
+    context = {
+        'properties': properties
+    }
+    
+    return render(request, 'properties/property_list.html', context)
